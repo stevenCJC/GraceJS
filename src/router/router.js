179db851a9eb0545fe0517611112bb715fd92e33
Steven <steven_chen@thedspway.com>
2014-03-20 19:56:22 +0800

    Signed-off-by: Steven <steven_chen@thedspway.com>

--- src/router/router.js
+++ src/router/router.js
@@ -1,27 +1,36 @@
-define([], function() {
+define(['function/urlData'], function(urlData) {
+	/*
+	localhost#view!1,2,3/tab!noteAll
+	hashChange事件触发后会对比新旧hash每个单位的改变
+	有新增或者改变
 	
-	function Router(){
+	*/
+	function Router(callback){
 		this.routers={};
 		//绑定hash改变事件
+		var that=this;
 		window.addEventListener('hashchange',function(e){
-			hashRouter(e);
+			hashRouter(e,callback);
 		},false);
 	}
+	Router.prtotype={
+		constructor:Router,
+	}
 	
-	
-	function hashRouter(e){
+	function hashRouter(e,callback){
 		//下面两个变量暂无作用
+		var subs,index,subs,param,h,hash,hashs,oldHash,hashChanges={};
 		var newURL=e.newURL;
 		var oldURL=e.oldURL;
-		//去除#，返回反编码后的路径
-		var hash;
+		index=oldURL.indexOf('#');
+		hash=oldURL.substr(index);
+		oldHash=hash.split('/');
 		if(window.location.hash)hash=decodeURI(window.location.hash).substr(1);
-		if(hash) var hashs=hash.split('/');//如果有hash则以/分拆路径
+		if(hash) hashs=hash.split('/');//如果有hash则以/分拆路径
 		else return;//如果没有hash则停止执行
-		var subs,index,subs,param,h;
+		
 		while(h=hashs.pop()){
 			if(h.indexOf('!')>-1) {
-				//分拆订阅名称和参数
 				index=h.indexOf('!');
 				subs=h.substr(0,index);
 				param=h.substr(index+1);
@@ -29,12 +38,21 @@ define([], function() {
 				subs=h;
 			}
 			
-			
-			//这里需要扩展param的解析函数
-			G.MD.publish(subs,param);
-			
+			aHash:for(var i=0,len=oldHash;i<len&&oldHash[i];i++){
+				if(oldHash[i].indexOf(subs+'!')==0){
+					if(oldHash[i].indexOf('='+param)!=oldHash[i].indexOf('!')){
+						//改变的触发器
+						hashChanges[subs]=urlData(param);
+						continue aHash;
+					}else continue aHash;
+				}
+				//新增的触发器
+				if(i==len-1) hashChanges[subs]=urlData(param);
+			}
 			
 		}
+		
+		callback(hashChanges,newURL,oldURL);
 		
 	}
 	
