e46c8e9deb480eb4bbc6b77f788a3beafca7c87c
stevenCJC <522702986@qq.com>
2015-02-05 21:54:16 +0800

    Signed-off-by: stevenCJC <522702986@qq.com>

---
+++ Gracejs 3.0/mvx/common/lifecycle.js
@@ -0,0 +1,25 @@
+define(['g','_/is','dom/core','dom/cud'], function (g) {
+	
+	var css={
+		__blacklist__:['_cssInit'],
+		_cssInit:function(){
+			if(this.Css){
+				if(g.is.array(this.Css)) 
+					for (var i=0,l=this.Css.length;i<l;i++)
+						this.loadCss(this.Css[i]);
+				else this.loadCss(this.Css);
+			}
+		},
+		
+		loadCss:function(path){
+			var id=path.replace(/\W/ig,'');
+			if(!g.q('[_="'+id+'"]').length)
+				g.q('head').append('<link rel="stylesheet" _id="'+id+'" type="text/css" href="'+path+'">')
+		},
+		
+	};
+	
+	
+	
+	return css;
+});
